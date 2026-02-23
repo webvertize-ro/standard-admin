cab89de2d03ed6683bfc945593d130824a219390
webvertize-ro <contact@webvertize.ro>
2026-02-23 10:25:33 +0200

    further changes

--- api/deleteEntry.js
+++ api/deleteEntry.js
@@ -1,15 +1,15 @@
 import client from '../lib/mongodb.js';
 
 export default async function handler(req, res) {
-  if (req.method !== 'GET') {
+  if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
   const { id } = req.body;
 
+  console.log('the received id is: ', id);
+
   if (id) {
     return res.status(200).json({ message: 'id successfully received' });
   }
-
-  console.log('the received id is: ', id);
 }
